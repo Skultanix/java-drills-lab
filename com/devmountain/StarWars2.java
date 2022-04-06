f6c2c0673fdda3ec9351e7c52d032692ad7d49c4
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-06 16:18:42 -0600

    Part 1 complete.

---
+++ com/devmountain/StarWars2.java
@@ -0,0 +1,19 @@
+package com.devmountain;
+
+public class StarWars2 {
+    public StarWars2() {
+    }
+        //Part One
+        public static void main(String[] var0){
+            String var1 = "Jabba";
+            String var2 = "the Hutt";
+            Integer var3 = 600;
+            String var4 = "Nal Hutta";
+            Float var5 = 3.9f;
+            Integer var6 = 1358;
+            System.out.println("They call him " + var1 + " " + var2 + ".");
+            System.out.println(var1 + " of " + var4 + " was born in " + var3 + " BBY.");
+            System.out.println(var1 + " is " + var5 + " meters tall and weighs " + var6 + " kilos.");
+        };
+
+};
