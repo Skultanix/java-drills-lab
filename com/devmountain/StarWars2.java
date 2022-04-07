a7d91ce35b62012bec02d6c308fc08248fb10d70
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-07 17:37:11 -0600

    Part 3 complete.

--- com/devmountain/StarWars2.java
+++ com/devmountain/StarWars2.java
@@ -7,10 +7,10 @@ public class StarWars2 {
         public static void main(String[] var0){
             String var1 = "Jabba";
             String var2 = "the Hutt";
-            Integer var3 = 600;
+            int var3 = 600;
             String var4 = "Nal Hutta";
-            Float var5 = 3.9f;
-            Integer var6 = 1358;
+            float var5 = 3.9f;
+            int var6 = 1358;
             System.out.println("They call him " + var1 + " " + var2 + ".");
             System.out.println(var1 + " of " + var4 + " was born in " + var3 + " BBY.");
             System.out.println(var1 + " is " + var5 + " meters tall and weighs " + var6 + " kilos.");
@@ -22,7 +22,35 @@ public class StarWars2 {
             purse -= (purse *= 0.1f);
             System.out.println("Remaining balance in purse is: "+purse+".");
             //Part 3
-
-        };
-
+            String bladeColor = "Purple";
+            String handeColor = "Black";
+            if ((bladeColor.equals("Purple")) && (handeColor.equals("Black"))) {
+                System.out.println("This one's mine!");
+            } else {System.out.println("Pass");
+            };
+            //Ship
+            String shipName = "The Deathstar";
+            if (shipName.equals("Millenium Falcon")) {
+                System.out.println("Let's go!");
+            } else {
+                System.out.println("I need to hide somewhere.");
+            };
+            //Spaceport Cantina
+            float itemCost = 5.99f;
+            boolean isRecommended = false;
+            float drinkCost = 0.99f;
+            int drinkSize = 23;
+            if ((itemCost <= 5) && (isRecommended == true)) {
+                System.out.println(var1 + " will order the item.");
+            } else {
+                System.out.println(var1+" "+var2+ " will not order this paltry dish.");
+            };
+            if ((drinkCost <= 1) && (drinkSize > 24)) {
+                System.out.println(var1+" will have a drink.");
+            } else {
+                System.out.println(var1+" will not have this swill.");
+            };
+    };
 };
+
+
